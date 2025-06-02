6670eb725380a4b6238ec2ebcb7750b4f899460c
Udara San <udarassanjeewa@gmail.com>
2025-06-02 15:37:23 +0530

    save employee completed

--- EMS-FN/js/dashboard.js
+++ EMS-FN/js/dashboard.js
@@ -8,3 +8,33 @@ $(document).ready(function() {
         $('#welcome-message').text('Welcome, ' + email);
     }
 });
+
+$('#save-employee').on('click', function() {
+    var ename = $('#ename').val();
+    var enumber = $('#enumber').val();
+    var edepartment = $('#edepartment').val();
+    var estatus= $('#estatus').val();
+    var eaddress = $('#eaddress').val();
+    
+    $.ajax({
+        method: 'POST',
+        url: 'http://localhost:8080/EMS_Web_exploded/employee',
+        contentType: 'application/json',
+        data: JSON.stringify({
+            ename: ename,
+            enumber: enumber,
+            eaddress: eaddress,
+            edepartment: edepartment,
+            estatus: estatus
+        }),
+        success: function(response) {
+            console.log(response);
+            if (response.code === '200') {
+                alert('Employee saved successfully!');
+                window.location.reload(); // Reload the page to see the updated list
+            } else {
+                alert('Error: ' + response.message);
+            }
+        },
+    });
+});
