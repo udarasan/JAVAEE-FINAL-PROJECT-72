e793c589ed2f9ead62db40f463aa74391882f42c
Udara San <udarassanjeewa@gmail.com>
2025-06-02 13:42:40 +0530

    cors issue fix

---
+++ EMS/src/main/java/org/example/CorsFilter.java
@@ -0,0 +1,19 @@
+package org.example;
+
+import jakarta.servlet.*;
+import jakarta.servlet.annotation.WebFilter;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebFilter("/*")
+public class CorsFilter implements Filter {
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        HttpServletResponse response = (HttpServletResponse) servletResponse;
+        response.setHeader("Access-Control-Allow-Origin", "*");
+        response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE, PUT");
+        response.setHeader("Access-Control-Allow-Headers", "Content-Type");
+        filterChain.doFilter(servletRequest, servletResponse);
+    }
+}
